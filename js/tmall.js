08f125282d53e08ae04da628dd5cb384aebacd4e
dtid_3031858daef9eb1b <1399480969@qq.com>
2020-10-13 00:12:28 +0800

    0.12

--- js/tmall.js
+++ js/tmall.js
@@ -1,10 +1,10 @@
 var slide = document.getElementsByClassName("swiper-slide");
 var lbd = document.querySelectorAll(".lbd div");
-var z = document.querySelector(".swiper-slide")
-console.log(lbd);
+var z = document.querySelector(".swiper-container")
+console.log(z);
 var num = 0 ;
 
-var t = setInterval(function () {
+function move () {
     num++;
     if (num>slide.length-1) {
         num = 0;
@@ -17,27 +17,27 @@ var t = setInterval(function () {
     lbd[num].style.background="white"
 
     for (let i = 0; i < lbd.length; i++) {
-        lbd[i].onclick=function () {
-            // clearInterval(t)
-            for (let j = 0; j < lbd.length; j++) {
-                lbd[j].style.background="#000"
-                slide[j].style.opacity=0;
-            }
-            lbd[i].style.background="white";
-            slide[i].style.opacity=1;
-            console.log(slide[i]);  
+        lbd[i].onmouseover=function (){
+            setTimeout(function () {
+                for (let j = 0; j < lbd.length; j++) {
+                    lbd[j].style.background="#000"
+                    slide[j].style.opacity=0;
+                }
+                lbd[i].style.background="white";
+                slide[i].style.opacity=1;
+            },800)
+
         }
-        lbd.onmouseout=function () {
-            //   t=setInterval()
-              console.log(1);
-        }
+    }   
+}
+var t = setInterval(move,1000)
 
-        
-    }
-
-
-},1000)
-
+z.onmouseout=function () {
+    t = setInterval(move,1000);
+}
+z.onmouseover=function(){
+    clearInterval(t)
+}
 
 
 
@@ -46,9 +46,9 @@ var topss = document.getElementById("topss");
 var leftk = document.querySelector("#leftk")
 var left = document.querySelectorAll("#leftk li");
 
-for (let i = 0; i < left.length; i++) {
-    console.log(left[i]);
-}
+// for (let i = 0; i < left.length; i++) {
+//     console.log(left[i]);
+// }
 window.onscroll=function () {
     console.log(document.documentElement.scrollTop);
     if (document.documentElement.scrollTop>800) {
@@ -68,63 +68,125 @@ window.onscroll=function () {
         if (document.documentElement.scrollTop>=arr[i]&&document.documentElement.scrollTop<=arr[i+1]) {
             for(var j=0;j<left.length;j++){
                 left[j].style.background="rgba(0,0,0,.6)";
-                console.log(left[j]);
+                // console.log(left[j]);
             }
             left[i].style.background=left[i].getAttribute("color");
-            console.log(left[i]);
+            // console.log(left[i]);
         }
     }
-
+//1192  1892 2592 3392 4092   4792 5392
     for (let i = 0; i < 4; i++) {
-        if (document.documentElement.scrollTop>2300) {
+        if (!imgz[i].flag) {
+        if (document.documentElement.scrollTop>1192) {
             imgz[i].src=imgsrc;
+            imgz[i].flag=true;
         }
+    }
     }
     for (let i = 4; i < 12; i++) {
-        if (document.documentElement.scrollTop>3300) {
+        if (!imgz[i].flag) {
+        if (document.documentElement.scrollTop>1892) {
             imgz[i].src=imgsrc;
         }
+    }
     }
     for (let i = 12; i < 20; i++) {
-        if (document.documentElement.scrollTop>4000) {
+        if (!imgz[i].flag) {
+        if (document.documentElement.scrollTop>2592) {
             imgz[i].src=imgsrc;
         }
+    }
     }
     for (let i = 20; i < 28; i++) {
-        if (document.documentElement.scrollTop>4000) {
+        if (!imgz[i].flag) {
+        if (document.documentElement.scrollTop>3392) {
             imgz[i].src=imgsrc;
         }
+    }
     }
     for (let i = 28; i < 36; i++) {
-        if (document.documentElement.scrollTop>4600) {
+        if (!imgz[i].flag) {
+        if (document.documentElement.scrollTop>4792) {
             imgz[i].src=imgsrc;
         }
+    }
     }
     for (let i = 36; i < imgz.length; i++) {
-        if (document.documentElement.scrollTop>6000) {
+        if (!imgz[i].flag) {
+        if (document.documentElement.scrollTop>5392) {
             imgz[i].src=imgsrc;
         }
     }
+    }
+      
 
 }
 
 var arr= [0,1800,2500,3300,4000,4600,5341,5341,6041];
 for (let i = 0; i < left.length; i++) {   
     left[i].onclick=function(){
-       document.documentElement.scrollTop=arr[i]
+    //    document.documentElement.scrollTop=arr[i];
+    if (animate) {
+        animate(document.documentElement,{scrollTop:arr[i]},1000,Tween.Linear);
+    }else{
+        return
+    }
     }
 }
 
 
 var imgz = document.querySelectorAll(".nte2 img");
-console.log(imgz);
+// console.log(imgz);
 var imgsrc ="./img/包包.jpg";
 
 
 
-// 4
-// 8
-// 8
-// 8
-// 8
-// 20
+var bigtop = document.querySelectorAll(".bigtop .tab-title")
+var bigtopzi2 = document.querySelector(".positi2")
+var bigtopzi = document.querySelector(".positi")
+var c= [bigtopzi,bigtopzi2]
+var n= 0;
+console.log(bigtop[1]);
+
+
+var ss= setInterval(function () {
+    n++
+    if (n>bigtop.length-1) {
+        n=0
+    }
+    for (let i = 0; i < bigtop.length; i++) {
+        bigtop[i].style.background="#fff";   
+        c[i].style.display="none"
+    }
+    bigtop[n].style.background="#00B262";
+    c[n].style.display="block"
+} ,1000)
+
+for (let i = 0; i < bigtop.length; i++) {
+    bigtop[i].onmouseover=function(){
+        clearInterval(ss);
+        for (let i = 0; i < bigtop.length; i++) {
+            bigtop[i].style.background="#fff";   
+            c[i].style.display="none"
+        }
+        c[i].style.display="block"
+        bigtop[i].style.background="#00B262";
+    }
+
+    bigtop[i].onmouseout=function(){
+        ss=setInterval(function () {
+            n++
+            if (n>bigtop.length-1) {
+                n=0
+            }
+            for (let i = 0; i < bigtop.length; i++) {
+                bigtop[i].style.background="#fff";   
+                c[i].style.display="none"
+            }
+            bigtop[n].style.background="#00B262";
+            c[n].style.display="block"
+        } ,1000)
+    }
+       
+    
+}
